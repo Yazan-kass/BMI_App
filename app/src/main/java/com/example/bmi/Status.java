b1496ebe373e345ffcd1e6ec45f95982694649e1
yazan kass <y.j.kass@gmail.com>
2021-12-14 15:58:19 +0200

    View Food List & Edit food details &part OOP & part Firebase

---
+++ app/src/main/java/com/example/bmi/Status.java
@@ -0,0 +1,53 @@
+package com.example.bmi;
+
+public class Status {
+    private String gender;
+    private int weight;
+    private int length;
+    private int age;
+    private String date;
+
+
+    public Status() {
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
